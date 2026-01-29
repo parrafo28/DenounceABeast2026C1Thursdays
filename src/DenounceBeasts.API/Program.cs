d8b892022f43ab4d408f98df5ee7b54fbca8d806
Starling Germosen <sgermosen@outlook.com>
2026-01-29 19:12:19 -0400

    Creation of the startup solution and project

---
+++ src/DenounceBeasts.API/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+//var ironIcrKey = "jkashdakjhsdjkashdkjagskdgajksdgkajsgdjagsdjkagsd";
+//var ironOcr = builder.Configuration.GetSection("IronOcr");
+//builder.Services.AddIronIcr(ironIcrKey);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
