27bd6a39720b47bbff4b7e484cefb96e63426119
Starling Germosen <sgermosen@outlook.com>
2026-01-29 20:42:11 -0400

    Creating the sectors controller and entity

---
+++ src/DenounceBeasts.API/Models/Sector.cs
@@ -0,0 +1,10 @@
+﻿namespace DenounceBeasts.API.Models
+{
+    public class Sector
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
